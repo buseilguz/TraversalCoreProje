ebf7b6a42512b703350965202fb0a81164bd62f2
buseilguz <buse.ilguz@gmail.com>
2024-11-12 18:46:43 +0300

    Entity'e özel List metodu tanımlama ve include

--- BusinessLayer/Concrete/ReservationManager.cs
+++ BusinessLayer/Concrete/ReservationManager.cs
@@ -41,9 +41,20 @@ namespace BusinessLayer.Concrete
             throw new NotImplementedException();
         }
 
-        public List<Reservation> GetListApprovalReservation(int id)
+        public List<Reservation> GetListWithReservationByWaitApproval(int id)
         {
-            return _reservationDal.GetListByFilter(x => x.AppUserId == id);
+           return _reservationDal.GetListWithReservationByWaitApproval(id);
+        }
+
+        public List<Reservation> GetListWithReservationByAccepted(int id)
+        {
+            return _reservationDal.GetListWithReservationByAccepted(id);
+
+        }
+
+        public List<Reservation> GetListWithReservationByPrevious(int id)
+        {
+            return _reservationDal.GetListWithReservationByPrevious(id);
         }
     }
 }
--- DataAccessLayer/Abstract/IReservationDal.cs
+++ DataAccessLayer/Abstract/IReservationDal.cs
@@ -7,6 +7,8 @@ namespace DataAccessLayer.Abstract
 {
    public interface IReservationDal:IGenericDal<Reservation>
     {
-
+        List<Reservation> GetListWithReservationByAccepted(int id);
+        List<Reservation> GetListWithReservationByWaitApproval(int id);
+        List<Reservation> GetListWithReservationByPrevious(int id);
     }
 }
